842fc37394397409f1711c611aec7826a32f5596
vova.dmytriukhin <volodymyr.dmytriukhin@dev.pro>
2021-02-10 19:00:02 +0200

    initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+package test.java;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    @Test
+    public void FirstTestMethod() {
+        System.out.println("Hello Test");
+    }
+}
