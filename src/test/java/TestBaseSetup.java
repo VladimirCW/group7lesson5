a14e895d18bd72e82e58a4e6a4ea68829a990fa7
vova.dmytriukhin <volodymyr.dmytriukhin@dev.pro>
2021-03-03 22:12:09 +0200

    initial commit

--- src/test/java/TestBaseSetup.java
+++ src/test/java/TestBaseSetup.java
@@ -3,11 +3,15 @@ package test.java;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import test.java.helpers.Screenshot;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class TestBaseSetup {
 
@@ -18,7 +22,14 @@ public class TestBaseSetup {
         System.setProperty("webdriver.chrome.driver", "chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--window-size=1300,1080");
-        driver = new ChromeDriver(chromeOptions);
+
+
+        //driver = new ChromeDriver(chromeOptions);
+        try {
+            driver = new RemoteWebDriver(new URL("http://ec2-3-133-59-40.us-east-2.compute.amazonaws.com:4444/wd/hub"), chromeOptions);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         iTestContext.setAttribute("driver", driver);
     }
 
